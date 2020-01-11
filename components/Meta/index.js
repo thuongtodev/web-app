46844f9a07ffe9afa3f92734190aebaabdd60644
Thuong To Thanh <thuongtothanh@MacBook-Pro-cua-Thuong.local>
2020-01-11 08:41:54 +0700

    add bootstrap

--- components/Meta/index.js
+++ components/Meta/index.js
@@ -6,6 +6,12 @@ const Meta = () => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
+        <link
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous"
+        />
       </Head>
       <style jsx global>{`
         body {
