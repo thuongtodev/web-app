3ece05c6a7413815f96eb520993a75b43ef584a6
Thuong To Thanh <thuongtothanh@MacBook-Pro-cua-Thuong.local>
2020-01-11 08:25:47 +0700

    done header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,24 +1,68 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import Link from 'next/link'
+import { FaSearch, FaShareAlt } from 'react-icons/fa'
 import styles from './styles.scss'
 import logo from './assets/logo.svg'
+import en from './assets/en.png'
+import zh from './assets/zh.png'
+import Dropdown from './components/Dropdown'
+import { reducer } from '../../utils/functions'
 
 const Header = () => {
+  const initState = {
+    languages: [
+      {
+        id: 0,
+        imgUrl: en,
+        selected: true,
+        title: 'en',
+      },
+      {
+        id: 1,
+        imgUrl: zh,
+        selected: false,
+        title: 'zh',
+      },
+    ],
+  }
+
+  const [state, setState] = useReducer(reducer, initState)
+
   return (
     <header className={styles.header}>
       <div className={styles.wrapper}>
         <div className={styles.headerContainer}>
           <div className={styles.leftContent}>
-            <div className={styles.logo}>
-              <Link href="/">
-                <a>
-                  <img src={logo} alt="logo" />
-                </a>
-              </Link>
+            <Link href="/">
+              <img src={logo} alt="logo" className={styles.logo} />
+            </Link>
+            <div className={styles.headerMenu}>
+              <a className={styles.a}>NEWS</a>
+            </div>
+            <div className={styles.headerMenu}>
+              <a className={styles.a}>CONFERENCES</a>
+            </div>
+            <div className={styles.headerMenu}>
+              <a className={styles.a}>HOW TO BUY BSV</a>
+            </div>
+            <div className={styles.headerMenu}>
+              <a className={styles.a}>VENTURES</a>
             </div>
           </div>
 
-          <div className={styles.rightContent}>Right</div>
+          <div className={styles.rightContent}>
+            <button className={styles.buttonBuy}>Buy BSV</button>
+            <div className={styles.iconContainer}>
+              <FaSearch />
+            </div>
+
+            <div className={styles.iconContainer}>
+              <FaShareAlt />
+            </div>
+            <div>
+              <Dropdown list={state.languages} />
+            </div>
+          </div>
         </div>
       </div>
     </header>
---
+++ utils/functions.js
@@ -0,0 +1,4 @@
+export const reducer = (state, newState) => ({
+  ...state,
+  ...newState,
+})
