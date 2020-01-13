b4adba38242da7de57edc9448cf8b2b51c471386
Thuong To Thanh <thuongtothanh@MacBook-Pro-cua-Thuong.local>
2020-01-13 23:04:07 +0700

    done footer home

--- components/FooterHome/components/InfoForm/InfoForm.js
+++ components/FooterHome/components/InfoForm/InfoForm.js
@@ -37,6 +37,41 @@ const InfoForm = () => {
           className={styles.input}
         />
       </div>
+
+      <div className={styles.labelWrap}>
+        <label className={styles.formLabel}>
+          Yes, I want to receive updates via email
+        </label>
+        <div className={styles.checkboxRadio}>
+          <input
+            type="radio"
+            value="Newsletters, Conferences and Events"
+            className={styles.inputRadio}
+          />
+          <span>
+            <label className={styles.label}>
+              Newsletters, Conferences and Events
+            </label>
+          </span>
+        </div>
+
+        <div className={styles.checkboxRadio}>
+          <input
+            type="radio"
+            value="Newsletters Only"
+            className={styles.inputRadio}
+          />
+          <span>
+            <label className={styles.label}>Newsletters Only</label>
+          </span>
+        </div>
+      </div>
+
+      <div className={styles.buttonWrap}>
+        <button className={styles.button} type="submit">
+          Submit
+        </button>
+      </div>
     </div>
   )
 }
